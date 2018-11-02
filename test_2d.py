02da93d4431ece8ccc297fcde033f65fa78d84b7
Tri Nguyen <tnguy51@u.rochester.edu>
2018-11-02 00:12:52 -0400

    add test

--- test_2d.py
+++ test_2d.py
@@ -7,6 +7,7 @@ import argparse
 import glob
 
 # User-defined module
+import numpy as np
 import lib.io as lio
 import lib.correlation as lcorrelation
 
@@ -17,7 +18,7 @@ if __name__ == "__main__":
 
     # read in helper
     print('reading file')
-    fname_list = sorted(glob.glob("test_divide_*.pkl"))
+    fname_list = sorted(glob.glob("output/test/test_divide_*.pkl"))
     helper = None
     for i, fname in enumerate(fname_list):
         print("- %s" % fname)
@@ -25,7 +26,6 @@ if __name__ == "__main__":
             helper.add(lio.load(fname))
             continue
         helper = lio.load(fname)
-        import numpy as np
         helper.ztheta_d1r2 = np.copy(helper.ztheta_d1r2)
 
 
@@ -34,110 +34,46 @@ if __name__ == "__main__":
 
     theta = bins.bins('theta')
     theta = 0.5*(theta[:-1]+theta[1:])
+    sin_2 = np.sin(theta/2.)
+    cos_2 = np.cos(theta/2.)
     r = cosmo.z2r(bins.bins('z'))
     r = 0.5 * (r[:-1] + r[1:])
     s = np.linspace(0, 200, 51)
 
     ftheta = helper.ftheta
-    ztheta = helper.ztheta_d1r2
-    zztheta = helper.zztheta
-    z_distr = helper.z1_distr
-
-    rr = np.zeros((2, 50, 50))
-    dd = np.zeros((2, 50, 50))
-    dr = np.zeros((2, 50, 50))
+    ztheta = helper.ztheta_d1r2[0]
+    zztheta = helper.zztheta[0]
+    z_distr = helper.z1_distr[0]
 
     # calculate rr
-    # loop over cosmology models
-    print('')
-    print('calculate rr')
-
-    for i in range(2):
-
-        # calculate 2-d weight matrix
-        w = ftheta[:, None]*z_distr[i][None, :]
-        w = w.ravel()
-
-        sin_2 = np.sin(theta/2.)
-        cos_2 = np.cos(theta/2.)
-
-        # Calculate RR(sigma, pi)
-        for j, pt_r in enumerate(r):
-            # calculate distance
-            sigma = (pt_r + r[None, :])*sin_2[:, None]
-            sigma = sigma.ravel()
-
-            pi = np.abs(pt_r - r[None, :])*cos_2[:, None]
-            pi = pi.ravel()
-
-            hist, _, _ = np.histogram2d(sigma, pi,
-                                        bins    = (s, s),
-                                        weights = z_distr[i][j]*w)
-            rr[i] += hist
+    # create the weight matrix
+    rr = np.zeros((50, 50))
+    w = ftheta[:, None]*z_distr[None, :]
+    for k, pt_r in enumerate(r):
+        sigma = sin_2[:, None]*(pt_r + r[None, :])
+        pi = cos_2[:, None]*np.abs(pt_r - r[None, :])
+        hist, _, _ = np.histogram2d(sigma.ravel(), pi.ravel(),
+                                    bins=(s,s),
+                                    weights=w.ravel()*z_distr[k])
+        rr += hist
 
     # calculate dr
-    print('')
-    print('calculate dr')
+    dr = np.zeros((50, 50))
+    for k, pt_r in enumerate(r):
+        sigma = sin_2[:, None]*(pt_r + r[None, :])
+        pi = cos_2[:, None]*np.abs(pt_r - r[None, :])
+        hist, _, _ = np.histogram2d(sigma.ravel(), pi.ravel(),
+                                    bins=(s,s),
+                                    weights=ztheta.ravel()*z_distr[k])
+        dr += hist
 
-    for i in range(2):
+    dd = np.zeros((50, 50))
+    for k, pt_r in enumerate(r):
+        sigma = sin_2[:, None]*(pt_r + r[None, :])
+        pi = cos_2[:, None]*np.abs(pt_r - r[None, :])
+        hist, _, _ = np.histogram2d(sigma.ravel(), pi.ravel(),
+                                    bins=(s,s),
+                                    weights=zztheta[:, :, k].ravel())
+        dd += hist
 
-        w = ztheta[i].ravel()
-
-        sin_2 = np.sin(theta/2.)
-        cos_2 = np.cos(theta/2.)
-
-        # Calculate DR(s)
-        for j, pt_r in enumerate(r):
-            # calculate distance
-            sigma = (pt_r + r[None, :])*sin_2[:, None]
-            sigma = sigma.ravel()
-
-            pi = np.abs(pt_r - r[None, :])*cos_2[:, None]
-            pi = pi.ravel()
-
-            hist, _, _ = np.histogram2d(sigma, pi,
-                                        bins    = (s, s),
-                                        weights = w)
-            dr[i] += hist
-
-    # calculate dd
-    print('')
-    print('calculate dd')
-
-    for i in range(2):
-        for j, pt_theta in enumerate(theta):
-
-            sin_2 = np.sin(pt_theta/2)
-            cos_2 = np.cos(pt_theta/2)
-
-            for k, pt_r in enumerate(r):
-                w = zztheta[i, j, k]
-
-                # calculate distance
-                sigma = (pt_r + r)*sin_2
-                pi = np.abs(pt_r - r)*cos_2
-
-                hist, _, _ = np.histogram2d(sigma, pi,
-                                            bins    = (s, s),
-                                            weights = w)
-                dd[i] += hist
-
-    import seaborn as sns
-    import matplotlib.pyplot as plt
-
-    i = 0
-    rr = rr[0]
-    dd = dd[0]
-    dr = dr[0]
-    rr = rr/helper.norm_rr.reshape(-1, 1, 1)
-    dd = dd/helper.norm_dd.reshape(-1, 1, 1)
-    dr = dr/helper.norm_d1r2.reshape(-1, 1, 1)
-    rr = rr[i]
-    dd = dd[i]
-    dr = dr[i]
-
-    tpcf = dd - 2*dr + rr
-    tpcf = np.where(rr != 0, tpcf/rr, 0)
-
-    sns.heatmap(tpcf)
-    plt.show()
+    lio.save('test2d.pkl', {'rr': rr, 'dr': dr, 'dd': dd})
