b5e0ac3a6f646f4eeedf8f3fe7935947ae97d530
Tolga Yapici <tyapici@ur.rochester.edu>
2019-03-28 13:47:24 -0400

    some setup.py changes

--- setup.py
+++ setup.py
@@ -9,38 +9,19 @@ from __future__ import absolute_import, division, print_function
 import glob
 import os
 import sys
-import pip
 
 #
 # setuptools' sdist command ignores MANIFEST.in
 #
 from distutils.command.sdist import sdist as DistutilsSdist
 from setuptools import setup, find_packages
-from setuptools.command.install import install as InstallCommand
-from py.KITCAT import versioning as ver
+import py.KITCAT.versioning as ver
 
-class Install(InstallCommand):
-    """ Customized setuptools install command which uses pip."""
-    def run(self, *args, **kwargs):
-        try:
-            # for older versions of pip
-            from pip._internal import main
-        except:
-            # for newer versions of pip
-            from pip import main
-        main(['install', '.'])
-        InstallCommand.run(self, *args, **kwargs)
-
-def pip_install(package):
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
-    else:
-        pip._internal.main(['install', package])
-        
 #
 # Begin setup
 #
 setup_keywords = dict()
+
 #
 # THESE SETTINGS NEED TO BE CHANGED FOR EVERY PRODUCT.
 #
@@ -51,21 +32,19 @@ setup_keywords['author_email'] = 'tnguy51@u.rochester.edu, tyapici@ur.rochester.
 setup_keywords['license'] = 'BSD'
 setup_keywords['url'] = 'https://github.com/DESI-UR/KITCAT'
 setup_keywords['version'] = ver.get_version(out_type='string')
-
-print("Version is set to {}".format(ver.get_version(out_type='string')))
-
-"""
-# this is a dirty way of installing the dependencies
-# but when I let everything handled by pip/setuptools,
-# the wheel generation spawns thousands of processes and kills
-# the computer
-print("Installing necessary packages with pip")
-fid = open('requirements.txt')
-for line in fid:
-    print(line)
-    pip_install(line)
-"""
-
+setup_keywords['requires'] = ['Python (>3.5)']
+setup_keywords['zip_safe'] = False
+setup_keywords['use_2to3'] = False
+setup_keywords['packages'] = find_packages('py')
+setup_keywords['package_dir'] = {'': 'py'}
+setup_keywords['provides'] = [setup_keywords['name']]
+setup_keywords['install_requires'] = ['numpy>=1.13.1', 
+                                      'healpy>=1.11.0',
+                                      'configparser>=3.5',
+                                      'astropy>=1.2.1',
+                                      'scipy>=0.19.1',
+                                      'matplotlib==2',
+                                      'scikit-learn>=0.18.1']
 #
 # END OF SETTINGS THAT NEED TO BE CHANGED.
 #
@@ -78,17 +57,6 @@ for line in fid:
 if os.path.isdir('bin'):
     setup_keywords['scripts'] = [fname for fname in glob.glob(os.path.join('bin', '*'))
         if not os.path.basename(fname).endswith('.rst')]
-
-setup_keywords['provides'] = [setup_keywords['name']]
-setup_keywords['setup_requires'] = ['numpy>1.13.1']
-setup_keywords['install_requires'] = ['numpy>=1.13.1', 'healpy>=1.11.0', 'configparser>=3.5', \
-                                      'astropy>=1.2.1', 'scipy>=0.19.1', 'matplotlib>=2.0.0', \
-                                      'scikit-learn>=0.18.1']
-setup_keywords['zip_safe'] = False
-setup_keywords['use_2to3'] = True
-setup_keywords['packages'] = find_packages('py')
-setup_keywords['package_dir'] = {'': 'py'}
-#setup_keywords['cmdclass'] = {'install': Install,}
 
 # Run setup command.
 #
